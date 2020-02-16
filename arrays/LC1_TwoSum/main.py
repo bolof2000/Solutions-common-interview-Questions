fc94eae296eb45f413ee0067728cdb20ca31420f
bolof2000 <bolofbaba@gmail.com>
2020-02-15 20:00:42 -0700

    setup

--- arrays/LC1_TwoSum/main.py
+++ arrays/LC1_TwoSum/main.py
@@ -1,12 +1,13 @@
 class Solution:
+
     def twoSum(self, nums, target):
         d = {}
         result =[]
         for i,num in enumerate(nums):
-            temp = target=num
+            temp = target = num
             if temp in d:
                 result.append(d[temp])
                 result.append(i)
 
             d[num] =i
-    return result 
+        return result
